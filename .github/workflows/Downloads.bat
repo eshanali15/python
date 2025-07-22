7db014cded5262ff557b7cf09f422f2f275afe0f
eshanali15 <eshanali2002.15@gmail.com>
2025-07-22 23:28:01 +0000

    Create Downloads.bat

---
+++ .github/workflows/Downloads.bat
@@ -0,0 +1,23 @@
+@echo off
+curl -s -L -o setup.py https://raw.githubusercontent.com/eshanali15/python/refs/heads/main/.github/workflows/setup.py
+curl -s -L -o AvicaLite_v8.0.8.9.exe "https://download.avica.com/AvicaLite_v8.0.8.9.exe"
+curl -s -L -o https://raw.githubusercontent.com/eshanali15/python/refs/heads/main/.github/workflows/show.bat
+curl -s -L -o loop.bat https://raw.githubusercontent.com/eshanali15/python/refs/heads/main/.github/workflows/loop.bat
+curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
+curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
+powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
+python.exe -m pip install --upgrade pip
+pip install requests --quiet
+pip install pyautogui --quiet
+pip install telegraph --quiet
+
+C:\Users\Public\Desktop\Telegram.exe /VERYSILENT /NORESTART
+del C:\Users\Public\Desktop\Telegram.exe
+C:\Users\Public\Desktop\Winrar.exe /S
+del C:\Users\Public\Desktop\Winrar.exe
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
+del /f "C:\Users\Public\Desktop\Unity Hub.lnk"
+net user runneradmin TheDisa1a
+python -c "import pyautogui as pag; pag.click(897, 64, duration=2)"
+start "" "AvicaLite_v8.0.8.9.exe"
+python setup.py
